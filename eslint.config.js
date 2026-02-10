f3d83eed34032a939fb0d9bc22db03de057c076a
Alex Hernandez <alexandre_2420@outlook.com>
2026-02-10 17:10:56 -0500

    config: setup linter and pettier

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [['^@angular'], ['^@?\\w'], ['^src/app', '^\\.'], ['^.+\\.s?css$']],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {},
+  },
+]);
