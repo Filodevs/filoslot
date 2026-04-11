132a8b2f2e221cb6dd0b764ec49991addb6f922b
unknown <alexandre_2420@outlook.com>
2026-04-11 16:43:31 -0500

    fix url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
   production: true,
   api: {
-    baseUrl: 'https://filoslot-api.onrender.com/api/v1/',
+    baseUrl: 'https://filoslot-api.onrender.com/api/v1',
     timeout: 30000,
     auth: {
       login: '/auth/login',
